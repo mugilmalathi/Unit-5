256e0c89fa721db5aac1f8da5789c67e9ba420b2
Mugilarasan <95951189+mugilmalathi@users.noreply.github.com>
2022-03-03 19:12:12 +0530

    03 march assignment

---
+++ Week 1/03 Mar/react-intro/src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  const mobile = ["Android", "Black Berry", "iPhone", "Windows phone"];
+  const manu = ["samsung", "htc", "iPhone", "nokia"]
+
+  return (
+    <div className='App'>
+      <h1>Mobile Operating System</h1>
+      <ul>{
+       mobile.map ((e)=>(
+          <li className='list1'>{e}</li>
+        ))}
+      </ul>
+      <h1>Mobile Manufacturers</h1>
+      <ul>{
+       manu.map ((e)=>(
+          <li>{e}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+
+
+
+export default App;
